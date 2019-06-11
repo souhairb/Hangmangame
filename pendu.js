766831f30368bf0cfd4909333281ec9c07eeeffc
Souhair bouallala <souhairbouallala@gmail.com>
2019-06-11 18:09:45 +0200

    js wordToGuess started

--- pendu.js
+++ pendu.js
@@ -1,64 +1,103 @@
+//créer l'array avec les mots à deviner
 var wordsToGuess = [
-  "Awkward",
-  "Bagpipes",
-  "Banjo",
-  "Bungler",
-  "Croquet",
-  "Crypt",
-  "Dwarves",
-  "Fervid",
-  "Fishhook",
-  "Fjord",
-  "Gazebo",
-  "Gypsy",
-  "Haiku",
-  "Haphazard",
-  "Hyphen",
-  "Ivory",
-  "Jazzy",
-  "Jiffy",
-  "Jinx",
-  "Jukebox",
-  "Kayak",
-  "Kiosk",
-  "Klutz",
-  "Memento",
-  "Mystify",
-  "Numbskull",
-  "Ostracize",
-  "Oxygen",
-  "Pajama",
-  "Phlegm",
-  "Pixel",
-  "Polka",
-  "Quad",
-  "Quip",
-  "Rhythmic",
-  "Rogue",
-  "Sphinx",
-  "Squawk",
-  "Swivel",
-  "Toady",
-  "Twelfth",
-  "Unzip",
-  "Waxy",
-  "Wildebeest",
-  "Yacht",
-  "Zealous",
-  "Zigzag",
-  "Zippy",
-  "Zombie"
+  "awkward",
+  "bagpipes",
+  "banjo",
+  "bungler",
+  "croquet",
+  "crypt",
+  "dwarves",
+  "fervid",
+  "fishhook",
+  "fjord",
+  "gazebo",
+  "gypsy",
+  "haiku",
+  "haphazard",
+  "hyphen",
+  "ivory",
+  "jazzy",
+  "jiffy",
+  "jinx",
+  "jukebox",
+  "kayak",
+  "kiosk",
+  "klutz",
+  "memento",
+  "mystify",
+  "numbskull",
+  "ostracize",
+  "oxygen",
+  "pajama",
+  "phlegm",
+  "pixel",
+  "polka",
+  "quad",
+  "quip",
+  "rhythmic",
+  "rogue",
+  "sphinx",
+  "squawk",
+  "swivel",
+  "toady",
+  "twelfth",
+  "unzip",
+  "waxy",
+  "wildebeest",
+  "yacht",
+  "zealous",
+  "zigzag",
+  "zippy",
+  "zombie"
 ];
 
+//choisir un mot de la liste de façon aléatoire
 let randomIndex = Math.floor(Math.random() * wordsToGuess.length);
 var wordToGuess = wordsToGuess[randomIndex];
+console.log(wordToGuess);
 
+// variables créées pour mettre la lettre cliquée dans le bon compartiment
+var rightWord = [];
+var wrongWord = [];
 var wordUnderscore = [];
 
+//DOM MANIPULATION----------------------------------------------------------------------
+let docUnderscore = document.getElementsByClassName("wordUnderscore");
+
 function createUnderscore() {
+  //remplacer les lettres du mot tiré aléatoirement par des "underscores"
   for (let i = 0; i < wordToGuess.length; i++) {
     wordUnderscore.push("_");
   }
-  return wordUnderscore;
+  return wordUnderscore.join(" ");
+  //join() crée et renvoie une nouvelle chaîne de caractères en concaténant tous les éléments d'un tableau (ou d'un objet semblable à un tableau).
+  //La concaténation utilise la virgule ou une autre chaîne, fournie en argument, comme séparateur.
 }
-console.log(createUnderscore());
+
+//-------------------------------------------------------------------------------------------------
+// le joueur débute la partie
+// clique sur une lettre
+document.addEventListener("keypress", event => {
+  //var keycode = event.keyCode; // renvoie le numéro de la lettre pressée dans le clavier
+  let letterClick = String.fromCharCode(event.keyCode); //convertit le keycode en lettre
+  console.log(letterClick);
+
+  //si la lettre est correcte:
+  if (wordToGuess.indexOf(letterClick) > -1) {
+    function getAllIndexes() {
+      for (let i = 0; i < wordToGuess.length; i++) {
+        if (wordToGuess[i] === letterClick) wordUnderscore[i] = letterClick; //remplace l'underscore par la lettre trouvée
+      }
+      return wordUnderscore;
+    }
+    getAllIndexes();
+    docUnderscore[0].innerHTML = getAllIndexes().join(" ");
+
+    alert("Bravo!");
+  } else {
+    alert("Oups!");
+  }
+});
+
+console.log(docUnderscore);
+docUnderscore[0].innerHTML = createUnderscore();
